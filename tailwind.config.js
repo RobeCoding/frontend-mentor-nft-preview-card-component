be91f4f555f157805e82c1d16e7943d1850070a2
RobeCoding <155230155+RobeCoding@users.noreply.github.com>
2024-01-05 21:26:22 +0100

    Nft Preview Component

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    colors:{
+      "SoftBlue":"hsl(215, 51%, 70%)",
+      "Cyan": "hsl(178, 100%, 50%)",
+      "VeryDarkBlueMain":"hsl(217, 54%, 11%)",
+      "VerydarkblueCard":"hsl(216, 50%, 16%)",
+      "VeryDarkBlueLine":"hsl(215, 32%, 27%)",
+      "White": "hsl(0, 0%, 100%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
